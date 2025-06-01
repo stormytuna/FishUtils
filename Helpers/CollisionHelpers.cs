dac56f432f0ac6bd4d1740687fa4f5218bfb2ea9
stormytuna <stormytuna@outlook.com>
2025-06-01 23:46:48 +0100

    Add BounceOffTiles method

--- Helpers/CollisionHelpers.cs
+++ Helpers/CollisionHelpers.cs
@@ -13,4 +13,26 @@ public static class CollisionHelpers
 	public static bool CanHit(Vector2 source, Vector2 target) {
 		return Collision.CanHit(source, 1, 1, target, 1, 1);
 	}
+
+	/// <summary>
+	/// Bounces off the tiles, given the current and old velocities.
+	/// </summary>
+	/// <param name="currentVelocity">The current velocity of the entity.</param>
+	/// <param name="oldVelocity">The old velocity of the entity before it touched the tile.</param>
+	/// <param name="xMult">An optional float that multiplies the X component of the velocity when bouncing off the tile. Defaults to 1f.</param>
+	/// <param name="yMult">An optional float that multiplies the Y component of the velocity when bouncing off the tile. Defaults to 1f.</param>
+	/// <returns>The new velocity of the entity after bouncing off the tile.</returns>
+	public static Vector2 BounceOffTiles(this Vector2 currentVelocity, Vector2 oldVelocity, float xMult = 1f, float yMult = 1f) {
+		Vector2 result = currentVelocity;
+		
+		if (currentVelocity.X != oldVelocity.X) {
+			result.X = -oldVelocity.X * xMult;	
+		}
+
+		if (currentVelocity.Y != oldVelocity.Y) {
+			result.Y = -oldVelocity.Y * yMult;	
+		}
+
+		return result;
+	}
 }
