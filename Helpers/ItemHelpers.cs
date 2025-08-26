400c2f1fac327b095f3b51ef380edfcecb2f556b
stormytuna <stormytuna@outlook.com>
2025-08-26 01:44:21 +0100

    add defaulttoyoyo method

--- Helpers/ItemHelpers.cs
+++ Helpers/ItemHelpers.cs
@@ -28,5 +28,33 @@ public static class ItemHelpers
 	/// <returns>true if the item is a mana pickup, false otherwise.</returns>
 	public static bool IsManaPickup(this Item item) {
 		return item.type is ItemID.Star or ItemID.SoulCake or ItemID.SugarPlum;	
-	}	
+	}
+
+	/// <summary>
+	/// Sets the given item to have default values for a yoyo item.
+	/// </summary>
+	/// <param name="item">The item to set the defaults for.</param>
+	/// <param name="damage">The damage of the yoyo.</param>
+	/// <param name="knockback">The knockback of the yoyo.</param>
+	/// <param name="crit">The critical strike chance of the yoyo.</param>
+	/// <param name="projectileType">The yoyo projectile.</param>
+	/// <param name="shootSpeed">The item shoot speed.</param>
+	public static void DefaultToYoyo(this Item item, int damage, float knockback, int crit, int projectileType, float shootSpeed) {
+		item.width = 24;
+		item.height = 24;
+
+		item.useStyle = ItemUseStyleID.Shoot;
+		item.useTime = item.useAnimation = 25;
+		item.channel = true;
+		item.noMelee = true;
+		item.noUseGraphic = true;
+		item.UseSound = SoundID.Item1;
+
+		item.damage = damage;
+		item.DamageType = DamageClass.MeleeNoSpeed;
+		item.knockBack = knockback;
+		item.crit = crit;
+		item.shoot = projectileType;
+		item.shootSpeed = shootSpeed;
+	}
 }
